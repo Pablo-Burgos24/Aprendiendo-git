c00bf83e7865d5bca4cbe5d1881b18c42e2a3cd3
Pablo Burgos <pablo3burgos@gmail.com>
2023-08-12 02:28:11 -0300

    agrgando juego de pidra papel o tijera al archivo .js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Aprendiendo GIT");
+let playerSelection = prompt("Ingrese su movimiento").toLowerCase();
+let computerSelection = ["rock", "paper", "sicissors"];
+computerSelection = computerSelection[Math.floor(Math.random() * 3)];
+
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection == playerSelection) {
+    return alert(`EMPATARON! | Jugador: ${playerSelection} / Maquina: ${computerSelection}`);
+  } else if (
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "rock" && playerSelection == "sicissors") ||
+    (computerSelection == "sicissors" && playerSelection == "paper")
+  ) {
+    return alert(`Gano la maquina | Jugador: ${playerSelection} / Maquina: ${computerSelection}`);
+  } else {
+    return alert(`Gano el jugador | Jugador: ${playerSelection} / Maquina: ${computerSelection}`);
+  }
+}
+playRound(playerSelection, computerSelection);
